0f5244469e695b992b9bc0328e5046574353f2ee
Daniel Fernandez Rico <deferrico@gmail.com>
2018-03-27 17:18:09 +0200

    :robot: Telegram bot interface ready

--- air.js
+++ air.js
@@ -1,52 +1,76 @@
 const ColorThief = require('color-thief-jimp');
 const Jimp = require('jimp');
 const http = require('http');
-var drawInIterm = require('iterm2-image');
+const drawInIterm = require('iterm2-image');
 
 // geodata from: http://www.mambiente.munimadrid.es/sica/scripts/index.php
 
-zones = {
-    '47': 'Mendez Alvaro',
-    '49': 'Retiro',
-    '08': 'Escuelas Aguirre',
-    '04': 'Plaza de España',
-    '35': 'Plaza del Carmen'
-    //TODO: scrapear el resto
+const zones = {
+  '47': 'Mendez Alvaro',
+  '49': 'Retiro',
+  '08': 'Escuelas Aguirre',
+  '04': 'Plaza de España',
+  '35': 'Plaza del Carmen'
+  // TODO: scrapear el resto
 }
 
-station = '08'
+function airmon(station = '08') {
+  // Calling from node, not as import:
+  if(process.argv[1].indexOf('air.js') != -1){
 
-if(!process.argv[2]) console.log("\nEstaciones disponibles: %s. Por defecto: %s", 
-    Object.values(zones).join(', '), zones[station]);
-else station = process.argv[2]
+    // We can pass station as argument
+    if(!process.argv[2]) {
+      console.log("\nEstaciones disponibles:\n%s.\nPor defecto: %s",
+        Object.values(zones).join(', '), zones[station]);
+    }
+    else station = process.argv[2]
 
-console.log('\nGetting data from station %s - %s\n', station, zones[station])
+    detail(station, draw=true)
+    console.log('\nGetting data from station %s - %s\n', station, zones[station])
+  }
 
-imageURL = 'http://www.mambiente.munimadrid.es/sica/datos/marker.'+station+'.png';
-detail = 'http://www.mambiente.munimadrid.es/sica/datos/indice.'+station+'.png';
+  let imageURL = 'http://www.mambiente.munimadrid.es/sica/datos/marker.' + station + '.png';
 
-Jimp.read(imageURL, (err, sourceImage) => {
+  Jimp.read(imageURL, (err, sourceImage) => {
     if (err) {
-        console.error('err1: '+err);
-        return;
+      console.error('err1: ' + err);
+      return;
     }
 
-    try{
-        var dominantColor = ColorThief.getColor(sourceImage);
-        let [r,g,b] = dominantColor
+    try {
+      var dominantColor = ColorThief.getColor(sourceImage);
+      let [r, g, b] = dominantColor
+      let text = ""
 
-        if(g>r && g>b) console.log('\x1b[32m%s\x1b[0m', '\nAir quality is fine');
-        else console.log(dominantColor)
-        //TODO: check other color codes
+      // TODO: return promise with result
+      if (g > r && g > b)
+        return('\x1b[32m%s\x1b[0m', '\nAir quality is fine');
+      else if (r == g)
+        return('\x1b[33m%s\x1b[0m', '\nAir quality could be better');
+      else return('No idea, code is: %s', String(dominantColor));
+      //TODO: check other color codes
 
-        // Detail img
-        http.get(detail, function(res) {
-            if (res.statusCode === 200) {
-                drawInIterm(res, function () {});
-            }
-        });
-
-    } catch(err){
-        console.log('err: '+err)
+    } catch (err) {
+      console.log('err: ' + err)
     }
-});
+  });
+}
+
+function detail(station, draw=false) {
+  let detailURL = 'http://www.mambiente.munimadrid.es/sica/datos/indice.' + station + '.png';
+
+  if(draw){
+    http.get(detailURL, function(res) {
+        if (res.statusCode === 200) {
+            drawInIterm(res, function () {});
+        }
+    });
+  }
+
+  return detailURL
+}
+
+airmon()
+
+exports.air = airmon;
+exports.graph = detail;
---
+++ bot.js
@@ -0,0 +1,65 @@
+const airjs = require('./air.js');
+const airmon = airjs.air;
+const graph = airjs.graph;
+
+const scrapper = require('./scrapper.js')
+const particles = scrapper.particles;
+
+const TeleBot = require('telebot');
+const bot = new TeleBot(process.env.telebot);
+
+let data = {};
+
+let station = '49'
+airmon(station)
+graph(station)
+particles(station)
+
+bot.on(['/start', '/hello'], (msg) => {
+  text = 'Welcome! Send me your location (or not) \
+    if you want to receive air quality updates near you';
+
+  let replyMarkup = bot.keyboard([
+    [bot.button('location', 'Send my location')],
+    ['How about no']
+  ], {
+    resize: true
+  });
+
+  data.chat_id = msg.chat.id;
+
+  return bot.sendMessage(msg.from.id, text, {
+    replyMarkup
+  });
+});
+
+bot.on('location', msg => {
+
+  console.log(msg.location);
+  return bot.sendMessage(msg.from.id, 'Thanks for that!', {
+    replyMarkup: 'hide'
+  });
+
+});
+
+bot.on(/no/, (msg) => {
+  return bot.sendMessage(msg.from.id, 'Ok, send me the location later if you want to!', {
+    replyMarkup: 'hide'
+  });
+});
+
+// TODO: locate nearest station and set data.station
+// TODO: method to use detail img graph from airjs
+
+/*
+ * pseudo-cron
+ */
+bot.on('tick', () => {
+  if (!data.chat_id) return;
+
+  // text = airmon('49')
+
+  return bot.sendMessage(data.chat_id, text)
+});
+
+bot.start();
