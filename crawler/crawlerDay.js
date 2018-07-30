dff96223668ceaadcb48b3cfb62884af9afce3a3
Daniel Fernandez <dfdez@iMacExd.local>
2018-07-30 10:21:42 +0200

    :card_file_box:Particles-mongo

--- crawler/crawlerDay.js
+++ crawler/crawlerDay.js
@@ -5,10 +5,49 @@ const metjs = require("./meteo.js");
 const meteo = metjs.m;
 
 function buildData(table) {
+    Object.keys(table).map(k => {
+        db[k].push(table[k])
+    });
 
+    requests++;
+
+    if(requests === 2){
+        sendData();
+    }
 }
 
-particles();
+function sendData() {
+    Object.keys(db).map(k => console.log(db[k]));
+}
+
+let requests = 0;
+let db = { // 24 tables, each with 1 (new) row 
+    "8":  "[]",
+    "4":  "[]",
+    "11": "[]",
+    "16": "[]",
+    "17": "[]",
+    "18": "[]",
+    "24": "[]",
+    "27": "[]",
+    "35": "[]",
+    "36": "[]",
+    "38": "[]",
+    "39": "[]",
+    "40": "[]",
+    "47": "[]",
+    "48": "[]",
+    "49": "[]",
+    "50": "[]",
+    "54": "[]",
+    "55": "[]",
+    "56": "[]",
+    "57": "[]",
+    "58": "[]",
+    "59": "[]",
+    "60": "[]"
+};
+
+particles(buildData);
 meteo(buildData);
 
-// mongoDB section
