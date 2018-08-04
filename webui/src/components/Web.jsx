e746e8d63bfe6ab1cfc8bea31606959a17bb6715
Daniel Fernandez Rico <deferrico@gmail.com>
2018-08-04 20:49:31 +0200

    :earth_africa: simple leaflet map added to dashboard

--- webui/src/components/Web.jsx
+++ webui/src/components/Web.jsx
@@ -15,14 +15,26 @@ class Web extends React.Component {
     }
 
     componentDidMount(){
+        let map = L.map('mapid').setView([40.42, -3.7], 12.5);
+        let token = "pk.eyJ1IjoiZGZyIiwiYSI6ImNqa2ZhN2dscjA2dm8zdm8zMTRpYzFtb3MifQ.OonL77wXkCGpixjghGTolA";
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+            maxZoom: 18,
+            id: 'mapbox.light',
+            accessToken: token
+        }).addTo(map);
+
+        // let url = "js/coordinates.csv";
     }
 
     render() {
-
-
         return (
             <div>
                 <Header></Header>
+                <div className="card">
+                    <div id="mapid"></div>
+                </div>
             </div>
     );
     }
