b72c8d5671a0a88415c6de567313e26913f4cb83
Daniel Fernandez <deferrico@gmail.com>
2018-08-21 22:36:07 +0200

    :arrow_up::recycle: Upgrading and cleaning dependencies

--- server/server.js
+++ server/server.js
@@ -1,48 +1,8 @@
-const http = require('http');
-const HttpDispatcher = require('httpdispatcher');
-const fs = require('fs');
+const express = require("express");
 
-const PORT=3000;
-
-let dispatcher = new HttpDispatcher();
-
-function handleRequest(request, response){
-	try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function(){
-	console.log("Working on http://localhost:%s 🚀", PORT);
-});
-
-dispatcher.setStatic('resources');
-
-dispatcher.onDataPost = function(route, dataHandler) {
-	dispatcher.onPost(route, dataDispatch(dataHandler));
-}
-
-dispatcher.onDataGet = function(route, dataHandler) {
-    dispatcher.onGet(route, dataDispatch(dataHandler));
-}
-
-dispatcher.onGet("/page1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
-});
-
-dispatcher.onPost("/", function(req, res){
-	console.log(req.body);
-  console.log(req.headers);
-  res.writeHead(200, {'Content-Type': 'application/json'});
-
-	res.end(JSON.stringify(req.body));
-	return 0;
+const app = express();
+const port = process.env.PORT || 5656;
+// routes go here
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
 })
