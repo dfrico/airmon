6084a0fa44e13b9dd99f1f7e7340213a91d9023a
Daniel Fernandez <deferrico@gmail.com>
2018-09-23 15:00:12 +0200

    :white_check_mark: changed Jest methods for server integration test

--- server/server.test.js
+++ server/server.test.js
@@ -1,14 +1,14 @@
 const request = require('supertest');
 const app = require('./server.js');
 
-describe('Todos list API Integration Tests', () => {
-    describe('#GET / tasks', () => {
-        it('should get all tasks', (done) => {
+describe('Airmon API Integration Tests', () => {
+    describe('#GET /rest/api/status/', () => {
+        it('should get status', (done) => {
             request(app)
                 .get('/rest/api/status/')
                 .end((err, res) => {
-                    expect(res.statusCode).to.equal(200);
-                    expect(res.body).to.be.an('object');
+                    expect(res.statusCode).toEqual(200);
+                    expect(res.body).toBeInstanceOf(Object);
                     done();
                 });
         });
